53a26db0ab0805d47b6590ea928007bd7af2f914
Andrei Starichenko <a.starichenko@element-t.ru>
2023-11-28 11:42:31 +0300

    add separated skipped/skipped_danger

--- object_info/main.py
+++ object_info/main.py
@@ -10,7 +10,6 @@ pass
 TYPE_ELEMENTARY_SINGLE: tuple = (type(None), bool, str, bytes, int, float, )
 TYPE_ELEMENTARY_COLLECTION: tuple = (set, list, dict, )
 TYPE_ELEMENTARY: tuple = (*TYPE_ELEMENTARY_SINGLE, *TYPE_ELEMENTARY_COLLECTION, )
-print(f"{TYPE_ELEMENTARY=}")
 
 
 def _value_search_by_list(source=None, search_list=[]):
@@ -65,6 +64,27 @@ def type_is_elementary_single(source):
     return isinstance(source, TYPE_ELEMENTARY_SINGLE)
 
 
+names_miss_fullnames = [
+    # TEST
+    "attrMissFullName",
+    # GIT
+    "checkout", "detach",
+]
+
+
+names_danger_entries = [
+    # TEST
+    "MissPartName",
+
+    # MAIN
+    "init", "new", "create", "enter",
+    "set",
+    "clone", "copy", "move",
+    "next",
+    "close", "del", "exit", "clear", "reduce"
+]
+
+
 # =====================================================================================================================
 class ObjectInfo:
     source: Any = None
@@ -78,26 +98,33 @@ class ObjectInfo:
         if method - try to start it!
         """
         self.skipped = []
+        self.skipped_danger = []
+
         self.properties_ok = {}
-        self.properties_fail = {}
+        self.properties_exx = {}
         self.objects = {}
         self.methods_ok = {}
-        self.methods_fail = {}
+        self.methods_exx = {}
 
         print("="*50 + "print_object_info")
         print(f"str={str(self.source)}")
         print(f"repr={repr(self.source)}")
-        print("-"*50)
+        # print("-"*50)
 
         for name in dir(self.source):
-            if False:
+            # SKIP
+            if name in names_miss_fullnames:
                 self.skipped.append(name)
+                continue
+            if _value_search_by_list(source=name, search_list=names_danger_entries):
+                self.skipped_danger.append(name)
+                continue
 
             try:
                 attr_obj = getattr(self.source, name)
             except Exception as exx:
-                value = f"***EXCEPTION***{exx!r}"
-                self.properties_fail.update({name: value})
+                value = exx
+                self.properties_exx.update({name: value})
                 continue
 
             if callable(attr_obj):
@@ -105,8 +132,8 @@ class ObjectInfo:
                     value = attr_obj()
                     self.methods_ok.update({name: value})
                 except Exception as exx:
-                    value = f"***EXCEPTION***{exx!r}"
-                    self.methods_fail.update({name: value})
+                    value = exx
+                    self.methods_exx.update({name: value})
                 continue
 
             # print(f"{name=}/{attr_obj=}/type={type(attr_obj)}/elementary={isinstance(attr_obj, TYPE_ELEMENTARY)}")
@@ -118,14 +145,15 @@ class ObjectInfo:
                 value = attr_obj
                 self.objects.update({name: value})
 
-        for batch_name in ["properties_ok", "properties_fail", "objects", "methods_ok", "methods_fail"]:
+        for batch_name in ["properties_ok", "properties_exx", "objects", "methods_ok", "methods_exx"]:
             print("-" * 10 + f"[{batch_name}]" + "-" * 50)
             for name, value in getattr(self, batch_name).items():
                 print(f"{name:25}\t{value.__class__.__name__:10}:{value}")
 
-        print("-"*50 + "SKIPPED")
-        for name in self.skipped:
-            print(f"{name:25}\t: SKIPPED")
+        for batch_name in ["skipped", "skipped_danger"]:
+            print("-" * 10 + f"[{batch_name}]" + "-" * 50)
+            for name in getattr(self, batch_name):
+                print(name)
 
         print("="*50)
 
@@ -238,27 +266,14 @@ class ObjectInfo:
             if nested_level != 0 and attr_str == _parents_list[-1]:
                 continue
 
-            danger_entries_list = [
-                "init", "new", "create", "enter",
-                "set",
-                "clone", "copy", "move",
-                "next",
-                "close", "del", "exit", "clear", "reduce"
-                "close", "del", "exit", "clear", "reduce"
-            ]
-
-            miss_names_special = [
-                # GIT
-                "checkout", "detach",
-            ]
             if miss_names:
-                miss_names_special.extend(miss_names)
+                names_miss_fullnames.extend(miss_names)
 
             # get VALUE
-            if attr_str in miss_names_special:
+            if attr_str in names_miss_fullnames:
                 value = "***MISSED SPECIAL***"
 
-            elif _value_search_by_list(source=attr_str, search_list=danger_entries_list):
+            elif _value_search_by_list(source=attr_str, search_list=names_danger_entries):
                 value = "***MISSED DANGER***"
 
             else:
@@ -314,6 +329,8 @@ class Cls0:
 
 
 class Cls1:
+    attrMissFullName = "attrMissFullName"
+    attrMissPartName = "MissPartName"
     attrNone = None
     attrInt = 1
     attrFloat = 2
@@ -326,6 +343,9 @@ class Cls1:
     @property
     def propertyInt(self):
         return 1
+    @property
+    def propertyExx(self):
+        raise Exception("exxMsg")
     def methInt(self):
         return 1
     def methExx(self):
@@ -334,4 +354,58 @@ class Cls1:
 
 if __name__ == "__main__":
     ObjectInfo(Cls1()).print_object_info()
-    # ObjectInfo(Cls1())._print_object_info__deep()
+"""
+==================================================print_object_info
+str=<__main__.Cls1 object at 0x000001F6BFE13FE0>
+repr=<__main__.Cls1 object at 0x000001F6BFE13FE0>
+----------[properties_ok]--------------------------------------------------
+__dict__                 	dict      :{}
+__doc__                  	NoneType  :None
+__module__               	str       :__main__
+__weakref__              	NoneType  :None
+attrDict                 	dict      :{1: 1}
+attrFloat                	int       :2
+attrInt                  	int       :1
+attrList                 	list      :[1, 2, 3]
+attrListObj              	list      :[<__main__.Cls0 object at 0x000001F6BFE126C0>, <__main__.Cls0 object at 0x000001F6BFE13F20>, 1]
+attrNone                 	NoneType  :None
+attrSet                  	set       :{1, 2, 3}
+propertyInt              	int       :1
+----------[properties_exx]--------------------------------------------------
+propertyExx              	Exception :exxMsg
+----------[objects]--------------------------------------------------
+attrObj                  	Cls0      :<__main__.Cls0 object at 0x000001F6BFE12B70>
+----------[methods_ok]--------------------------------------------------
+__class__                	Cls1      :<__main__.Cls1 object at 0x000001F6BFE09070>
+__dir__                  	list      :['__module__', 'attrMissFullName', 'attrMissPartName', 'attrNone', 'attrInt', 'attrFloat', 'attrClass', 'attrObj', 'attrSet', 'attrList', 'attrDict', 'attrListObj', 'propertyInt', 'propertyExx', 'methInt', 'methExx', '__dict__', '__weakref__', '__doc__', '__new__', '__repr__', '__hash__', '__str__', '__getattribute__', '__setattr__', '__delattr__', '__lt__', '__le__', '__eq__', '__ne__', '__gt__', '__ge__', '__init__', '__reduce_ex__', '__reduce__', '__getstate__', '__subclasshook__', '__init_subclass__', '__format__', '__sizeof__', '__dir__', '__class__']
+__getstate__             	NoneType  :None
+__hash__                 	int       :134955799550
+__repr__                 	str       :<__main__.Cls1 object at 0x000001F6BFE13FE0>
+__sizeof__               	int       :16
+__str__                  	str       :<__main__.Cls1 object at 0x000001F6BFE13FE0>
+__subclasshook__         	NotImplementedType:NotImplemented
+attrClass                	Cls0      :<__main__.Cls0 object at 0x000001F6BFE09760>
+methInt                  	int       :1
+----------[methods_exx]--------------------------------------------------
+__eq__                   	TypeError :expected 1 argument, got 0
+__format__               	TypeError :Cls1.__format__() takes exactly one argument (0 given)
+__ge__                   	TypeError :expected 1 argument, got 0
+__getattribute__         	TypeError :expected 1 argument, got 0
+__gt__                   	TypeError :expected 1 argument, got 0
+__le__                   	TypeError :expected 1 argument, got 0
+__lt__                   	TypeError :expected 1 argument, got 0
+__ne__                   	TypeError :expected 1 argument, got 0
+methExx                  	Exception :exxMsg
+----------[skipped]--------------------------------------------------
+attrMissFullName
+----------[skipped_danger]--------------------------------------------------
+__delattr__
+__init__
+__init_subclass__
+__new__
+__reduce__
+__reduce_ex__
+__setattr__
+attrMissPartName
+==================================================
+"""
