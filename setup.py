c87105027dd428ed73dd5182250c409f4be066da
Andrei Starichenko <a.starichenko@element-t.ru>
2023-12-06 15:00:03 +0300

    ready v0.0.5

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as f:
 NAME = "object_info"
 
 setup(
-  version="0.0.4",
+  version="0.0.5",
   description="print info about object (attributes+properties+methods results)",
   keywords=[
     "object info",
