08845aeba3a548ac03eed1e086d9efa6459485f2
Andrei Starichenko <a.starichenko@element-t.ru>
2023-12-07 17:52:30 +0300

    zero add !_requirements_install.bat

---
+++ !_requirements__update.bat
@@ -0,0 +1,25 @@
+echo off
+cls
+
+echo ===1[UPDATE PIP]===
+pip install --upgrade pip
+echo _
+echo _
+echo _
+echo _
+echo _
+echo _
+echo _
+
+echo ===2[UPDATE ALL REQUIREMENTS]===
+pip install --upgrade -r requirements.txt
+echo _
+echo _
+echo _
+echo _
+echo _
+echo _
+echo _
+
+echo ===3[FINISH]===
+pause
